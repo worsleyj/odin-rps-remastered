61e5ccd28f4edbc352d1816fd27d3f58b235e417
worsleyj <josephworsley23@gmail.com>
2024-07-10 13:48:45 -0400

    add computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('test');
+function getComputerChoice() {
+    let randomSelection = Math.floor(Math.random() * 3);
+
+    if (randomSelection == 0) {
+        return "Rock";
+    } else if (randomSelection == 1) {
+        return "Paper";
+    } else if (randomSelection == 2) {
+        return "Scissors";
+    } else return "error"
+}
+
+
+console.log(getComputerChoice());
